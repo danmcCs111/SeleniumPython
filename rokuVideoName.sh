103e48a362910c9355dfe5a8f2062f2b17480d2f
danmcCs111 <danmccs111@gmail.com>
2025-11-09 13:46:42 -0500

    add prefix to url generation output

--- rokuVideoName.sh
+++ rokuVideoName.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 file=$1
-egrep -o "\/watch\/[a-zA-Z0-9\-]*\"" $file | sed 's/\"//g'
+egrep -o "\/watch\/[a-zA-Z0-9\-]*\"" $file | sed 's/\"//g' | sed 's/^/https\:\/\/therokuchannel.roku.com/g'
