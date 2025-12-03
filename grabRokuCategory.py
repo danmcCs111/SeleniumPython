5fbea40152795c8dd4d7616bacdc77697aa7df21
danmcCs111 <danmccs111@gmail.com>
2025-12-03 17:48:00 -0500

    adjust to perform scroll

--- grabRokuCategory.py
+++ grabRokuCategory.py
@@ -1,13 +1,27 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver import Keys, ActionChains
 import time
 import sys
 
-url = sys.argv[1]
-print(url)
+def enterUrl(driver, url):
+    driver.get(url)
+    time.sleep(5)
+    print(driver.page_source)
+    body = driver.find_element("tag name", "body") 
+    return body
 
-driver = webdriver.Chrome()
-driver.get(url)
-time.sleep(5)
-print(driver.page_source)
-driver.quit()
+def scrollDown(body):
+    body.send_keys(Keys.ESCAPE)
+    for i in range(7):
+        body.send_keys(Keys.PAGE_DOWN)
+        time.sleep(1)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    print(url)
+    driver = webdriver.Chrome()
+    body = enterUrl(driver, url)
+    scrollDown(body)
+    print(driver.page_source)
+    driver.quit()
